1681cbdf459daf12b63f0cb1df49695d637dd867
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2022-01-23 22:51:05 +0100

    test

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -6,13 +6,12 @@ const Navbar = () => {
   return (
       <div className={styles.navWrapper}>
         <div className={styles.logo}>
-          <Icon name="logo" />
-          <p>Orangeboat</p>
-          <Icon name="arrowDown" />
-        </div>
-        <div className={styles.radio}>
-              console.log("           ")
-    <hsfbh>             </djkjf>
+                    <Icon name="logo" />
+                    <p>Orangeboat</p>
+                    <Icon name="arrowDown" />
+                  </div>
+                  <div className={styles.radio}>
+
       </div>
         <div className={styles.user}>
           <Icon name="logo" />
