491baa023f2ffdea3d419b977c0ed86a78c1ccba
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2022-01-23 19:31:25 +0100

    Logo svg $ arrow down svg

---
+++ src/Components/Icons/stock/logo.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+import React from 'react';
+/**
+ * @param {Object} props Component props
+ * @returns {React.Component} React component
+ */
+const SVG = (props: unknown): unknown => (
+	<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect width="40" height="40" rx="10" fill="#F6F6F8"/>
+		<rect x="3.33334" y="3.33334" width="33.3333" height="33.3333" rx="7" fill="#E6821D"/>
+		<path d="M13.416 26.2199L14.0886 21.7065H15.907C17.576 21.7065 18.2984 22.3741 18.2984 23.7095C18.2984 25.1783 17.3518 26.2199 15.6579 26.2199H13.416ZM14.5619 18.3681L15.1597 14.4155H16.9533C18.3233 14.4155 18.971 14.923 18.971 16.1515C18.971 17.5936 18.1988 18.3681 16.5547 18.3681H14.5619Z" fill="white"/>
+		<path d="M20.3598 26.2199L21.0324 21.7065H22.8508C24.5198 21.7065 25.2422 22.3741 25.2422 23.7095C25.2422 25.1783 24.2956 26.2199 22.6017 26.2199H20.3598ZM21.5056 18.3681L22.1035 14.4155H23.897C25.2671 14.4155 25.9148 14.923 25.9148 16.1515C25.9148 17.5936 25.1426 18.3681 23.4985 18.3681H21.5056Z" fill="white"/>
+	</svg>
+
+);
+
+export default SVG;
