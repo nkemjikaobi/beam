8438605efabeea32ae79e9020584ba8dcf484fe3
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2022-01-24 02:35:15 +0100

    Performance comments

--- src/Components/PerformanceComments/PerformanceComments.tsx
+++ src/Components/PerformanceComments/PerformanceComments.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import styles from "./PerformanceComments.module.scss";
+import Icon from "../Icons/icon";
 
 const PerformanceComments = () => {
-  return <div className={styles.wrapper}>Comments</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <p>April 2020</p>
+        <div className={styles.header__right}>
+          <p>3.5</p>
+          <p>Exceeded expectations</p>
+          <Icon name="greenIndicator" />
+          <p>3.5%</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PerformanceComments;
